a68045676dd08d59565a854413359781019baf12
shimo0904 <7117137@asia-u.ac.jp>
2020-05-14 15:38:14 +0900

    更新:new view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
 before_action :set_item, only: [:show, :edit, :update]
 
+  def new
+  end
+
   private
   def set_item
     @item = Item.find(params[:id])
