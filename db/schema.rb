16ed9c1546441e5d4536662407357ba8637094db
yasunoriakita0521 <sonunder-0521aoi@softbank.ne.jp>
2020-05-17 22:03:23 +0900

    Revert "test"
    This reverts commit 522c78eeaefb94daae13edbc151d68b2bb72fe71.

--- db/schema.rb
+++ db/schema.rb
@@ -61,9 +61,6 @@ ActiveRecord::Schema.define(version: 2020_05_11_022422) do
     t.string "tel_number", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "password_confirmation", default: "", null: false
-    t.text "introduction"
-    t.string "user_image"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
