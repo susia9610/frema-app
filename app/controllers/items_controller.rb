36cd76a98e9504f3decdddf6ffc9b4594ff65027
come1120 <61959057+come1120@users.noreply.github.com>
2020-05-17 20:04:49 +0900

    Revert "item.controllers.delete"
    This reverts commit 54d8f58230f008349bf537e4138f642346ec4540.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,29 @@
 class ItemsController < ApplicationController
+  
+  def index
+    
+  end
+  
+  def new
+
+  end
+    
+  def create
+    
+  end
+
+  def edit
+
+  end  
+
+  def update
+   
+  end
+
+  def destroy
+   
+  end
+  
+  def show
     
   end
