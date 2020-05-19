88104245a8e84f3c2270447b68166541d4dcf502
yasunoriakita0521 <61900216+yasunoriakita0521@users.noreply.github.com>
2020-05-19 09:15:24 +0900

    Update and rename 20200510120251_create_users.rb to 20200510120251devise_create_users.rb

--- db/migrate/20200510120251devise_create_users.rb
+++ db/migrate/20200510120251devise_create_users.rb
@@ -1,4 +1,4 @@
-class CreateUsers < ActiveRecord::Migration[5.2]
+class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string   :nickname          , null: false
