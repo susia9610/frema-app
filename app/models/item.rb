1419651f7ffd6dfc38a4e5b69a7f5513c0e3fc9a
shimo0904 <7117137@asia-u.ac.jp>
2020-05-22 22:52:34 +0900

    追加:conditionモデル

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,7 @@ class Item < ApplicationRecord
   has_many   :images, dependent: :destroy
   validates :name, :description, :category, :condition, :postage, :prefecture, :shipping_days, :price, presence: true
   validates :price, numericality: { only_integer: true , greater_than: 0, less_than: 9999999 }
+
+  belongs_to_active_hash :condition
 end
 
