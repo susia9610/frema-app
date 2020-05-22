37dae414d7f751a8edd4a4d33d5aeb1705cd16b0
shimo0904 <7117137@asia-u.ac.jp>
2020-05-22 15:23:56 +0900

    追加:prefectureモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_022422) do
+ActiveRecord::Schema.define(version: 2020_05_22_053848) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "prefecture_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "url", null: false
