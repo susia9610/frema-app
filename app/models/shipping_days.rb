f999615918598d0c09b7ef683d7ba963f3082388
shimo0904 <7117137@asia-u.ac.jp>
2020-05-23 19:15:15 +0900

    追加:shipping_days selectbox

--- app/models/shipping_days.rb
+++ app/models/shipping_days.rb
@@ -1,5 +1,5 @@
 class Shipping_days < ActiveHash::Base
   self.data = [
-      {id: 1, name: "1〜２日で発送"}, {id: 2, name: "２〜3日で発送"},{id: 3, "４〜7日で発送"}
+      {id: 1, name: "1〜２日で発送"}, {id: 2, name: "２〜3日で発送"},{id: 3, name: "４〜7日で発送"}
   ]
 end
