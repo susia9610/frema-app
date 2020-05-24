8939f0e682cb7e74fb227d0eaae8fc23e4ffbfc5
shimo0904 <7117137@asia-u.ac.jp>
2020-05-24 15:15:02 +0900

    追加:categoryアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,8 @@ class Item < ApplicationRecord
   validates :name, :description, :category, :condition, :postage, :prefecture, :shipping_days, :price, presence: true
   validates :price, numericality: { only_integer: true , greater_than: 0, less_than: 9999999 }
 
+  belongs_to :category
+
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :condition
   belongs_to_active_hash :postage
