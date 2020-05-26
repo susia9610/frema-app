c9526435710554cf2c0a7f91bf2b4632ee6731c3
yasunoriakita0521 <sonunder-0521aoi@softbank.ne.jp>
2020-05-26 22:18:51 +0900

    addressモデルの登録　単体テスト

---
+++ spec/models/address_spec.rb
@@ -0,0 +1,59 @@
+requre 'rails_helper'
+
+describe Address do
+  describe '#create_address' do
+    
+    it "first_nameとlast_name_kanaとlast_name_kana、postal_code、prefecture_id、city、house_numberが存在すれば登録できること" do
+      address = build(:address) 
+      expect(address).to be_valid
+    end
+
+    it "first_nameがない場合は登録できないこと" do
+      address = build(:address, first_name: nil)
+      address.valid?
+      expect(address.errors[:first_name]).to include("cant't be blank")
+    end
+
+    it  "last_nameがない場合は登録できないこと" do
+      address = build(:address, first_name: nil)
+      address.valid?
+      expect(address.errors[:last_name]).to include("can't be blank")
+    end
+    
+    it "first_name_kanaがない場合は登録できないこと" do
+      address = build(:address, last_name_kana: nil) do
+      address.valid?
+      expect(address.errors[:first_name_kana]).to include("can't be blank") 
+    end
+
+    it "last_name_kanaがない場合は登録できないこと" do
+      address = build(:address, last_name_kana: nil)
+      address.valid?
+      expect(address.errors[:last_name_kana]).to include("can't be blank")
+    end
+    
+    it "postsl_codeがない場合は登録できないこと" do
+      address = build(:address, postsl_code: nil)
+      address.valid?
+      expect(address.errors[:postsl_code]).to include("can't be blank")
+    end 
+
+    it "prefecture_idがない場合登録できないこと" do
+      address = build{:address, prefecture_id: nil}
+      address.valid?
+      expect(address.errors[:prefecture_id]).to include("can't be blank")
+    end
+
+    it "cityがない場合は登録できないこと" do
+      address = build(:address, city: nil)
+      address.valid?
+      expect(address.errors[:city]).to include("can't be blank")
+    end
+
+    it "house_numbeerがない場合は登録できないこと" do
+      address = build(:address, house_number: nil)
+      address.valid?
+      expect(address.errors[:house_number]).to include("can7t be blank")
+    end 
+  end
+end
