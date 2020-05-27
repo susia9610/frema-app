f7e2d68432a41794fb31b17dbaacc5ced690a517
shimo0904 <7117137@asia-u.ac.jp>
2020-05-27 12:38:14 +0900

    追加:プレビュー画像を削除

--- app/assets/javascripts/new_item.js
+++ app/assets/javascripts/new_item.js
@@ -12,11 +12,22 @@ $(function(){
       var src = fileReader.result
       var html= `<div class='item-content'>
       <img src="${src}" width="114" height="80" class="item-content__icon">
-      <div class= 'item-content__delete'>削除</div>
+      <button class= 'item-content__delete'>削除</button>
       </div>`
 
       $(".new-wrapper__main__image-field").before(html);
     }
     fileReader.readAsDataURL(file);
   });
+
+  $(".new-wrapper__main").on("click", '.item-content__delete', function(e){
+    // e.stopPropagation();
+
+    var target_image = $(this).parent()
+    
+    target_image.remove();
+
+    // file_field.val("")
+  })
+
 });
