41efd14d8d40179f345fd6095b0c68d1dbdac0e3
susia9610 <61152544+susia9610@users.noreply.github.com>
2020-05-31 20:58:16 +0900

    メンター指摘事項修正

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -7,7 +7,7 @@ class AddressesController < ApplicationController
 
   def update
     @address = @user.address
-    @address.update(update_params)
+   
     unless @address.update(update_params)
       redirect_to edit_address_path(@address.id), notice: "変更できません。必須項目が入力されていません"
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-  #before_action :set_category
+  
   before_action :set_user_params, only: [:show, :edit, :update]
   before_action :set_user_current, only: [:index, :edit_detail, :update_detail]
 
   def index
-    #@items = @user.items
+    
   end
 
   def show
-    #@items = @user.items
+    
   end
 
   def edit
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   end
 
   def update_detail
-    @user.update(update_detail_params)
+    
     unless @user.update(update_detail_params)
       redirect_to user_edit_detail_path(@user.id), notice: "変更できません。メールアドレスとパスワードは必須項目です"
     end
@@ -40,9 +40,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def set_user_current
-    @user = User.find(current_user.id)
-  end
+  
 
   def update_params
     params.require(:user).permit(:nickname, :password, :encrypted_password)
