6da275e3c6ea233de3bc0424b837afe7ed74c0df
susia9610 <61152544+susia9610@users.noreply.github.com>
2020-06-02 21:50:01 +0900

    会員情報変更作業

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user_params, only: [:edit, :update]
   
 
   def index
@@ -14,4 +14,29 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def edit
+    
+  end
+
+  def update
+    @user = User.user
+   
+    unless @user.update(update_params)
+      redirect_to edit_user_path(@user.id), notice: "変更できません。必須項目が入力されていません"
+    end
+    sign_in(:user, @user)
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:familyname, :firstname, :familyname_kana, :firstname_kana, :email, :password, :birth_year, :birth_month, :birth_day)
+  end
+
+  def set_user_params
+    @user = User.find(params[:id])
+  end
+
+
+
+
 end
