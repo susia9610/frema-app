fe94ce798280d184ceabb9c93c357248670cb9fb
yasunoriakita0521 <sonunder-0521aoi@softbank.ne.jp>
2020-06-09 10:06:58 +0900

    コンフリクト解消

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_06_07_122834) do
+ActiveRecord::Schema.define(version: 2020_06_08_131314) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
