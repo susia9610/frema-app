562c49cc4612582d0e8536561ceee334f71a4336
yasunoriakita0521 <sonunder-0521aoi@softbank.ne.jp>
2020-06-09 10:08:42 +0900

    DB変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_122834) do
 ActiveRecord::Schema.define(version: 2020_06_08_131314) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
