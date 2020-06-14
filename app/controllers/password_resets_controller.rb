4f2c04c4e3bff766766fdfbed42c3f8837ecaacc
susia9610 <61152544+susia9610@users.noreply.github.com>
2020-06-14 21:23:11 +0900

    パスワードリセット

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,13 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
+  def new
+  end
+
+
+  
   def create
     @user = User.find_by(email: params[:password_resets][:email].downcase)
     if @user
@@ -12,6 +21,22 @@ class PasswordResetsController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password,  "can't be empty")
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   def create_reset_digest
     self.reset_token = User.new_token
     update_attribute(:reset_digest, User.digest(reset_token))
@@ -21,4 +46,29 @@ class PasswordResetsController < ApplicationController
   # パスワード再設定のメールを送信する
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+    
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # 正しいユーザーを確認する 
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
+    # 再設定用トークンが期限切れかどうかを確認する
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
+      end
+    end
 end
