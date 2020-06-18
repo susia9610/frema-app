fea3534eaaa9db7229e96d6016027cbe5e85b966
makyuuun <makyuuun@gmail.com>
2020-06-18 22:07:09 +0900

    move_toアクション削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :purchase, :done, :destroy, :pay]
   before_action :authenticate_user!, only:[:purchase, :pay, :done]
-  # before_action :move_to_root, except: [:index, :show]
   before_action :set_image, only:[:show, :purchase,:pay]
   before_action :set_card, only:[:purchase, :pay]
   require "payjp"
@@ -69,17 +68,13 @@ class ItemsController < ApplicationController
       customer: @card.customer_id,
       currency: 'jpy',
     )
-    # @item.update(status_id: BUYING_STATUS, buyer_id: current_user.id)
+    @item.update(status_id: BUYING_STATUS, buyer_id: current_user.id)
     redirect_to done_item_path
   end
   
   def done
   end  
 
-  # def move_to_root
-  #   redirect_to root_path unless user_signed_in?
-  #   flash[:alert] = 'ログインしてください'
-  # end
   def set_images
     @images = Image.where(item_id: params[:id])
   end
