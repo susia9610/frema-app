af66622db9404bcad921505c14d49a167a1ffc19
susia9610 <61152544+susia9610@users.noreply.github.com>
2020-06-21 22:47:36 +0900

    お気に入り機能

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,12 +1,13 @@
 class MypageController < ApplicationController
-  before_action :authenticate_user! , only: [:index, :logout, :new]##:cardは実装前のため、未だ反映していない
+  before_action :authenticate_user! , only: [:index, :logout, :new]
   
   def index
     @nickname = current_user.nickname
+    @favorites = Favorite.where(user_id: current_user.id)
+    @items = Item.all.includes(:images).where(status_id: "1").order(created_at: :desc)
+    #@items = @user.items 
   end
 
-  #def card
-  #end
 
   def show
   end
--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
   end
   
   resources  :items do
-    resources :favorites , only: [:create, :destroy]
+    resources :favorites , only: [:index, :create, :destroy]
   end
 
 
