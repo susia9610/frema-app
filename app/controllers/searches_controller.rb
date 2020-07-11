5da264df2886d45c0312a46f8fbf978b5d785007
susia9610 <61152544+susia9610@users.noreply.github.com>
2020-07-11 16:51:39 +0900

    商品検索レベル２

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,6 +5,8 @@ class SearchesController < ApplicationController
   def index
     @items = Item.search(params[:search]).limit(132)
     @search = params[:search]
+    @search_item = Item.ransack(params[:q]) 
+    @items = @search_item.result.page(params[:page])
   end
   
   def detail_search
