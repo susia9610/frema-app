2a58a6371ada620af6c9bc8543bac67f38caa561
susia9610 <61152544+susia9610@users.noreply.github.com>
2020-07-12 22:16:09 +0900

    コメントアウト削除

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,7 +10,6 @@ class SearchesController < ApplicationController
   end
   
   def detail_search
-    # @search = params[:q][:name_cont]
     @search_item = Item.ransack(params[:q]) 
     @items = @search_item.result.page(params[:page])
   end
@@ -23,20 +22,7 @@ class SearchesController < ApplicationController
     @parents = Category.where(ancestry: nil)
   end
   
-  # def detail_search_params
-  #   params.require(:q)
-  #   .permit(:name_cont,
-  #           :price_gteq, 
-  #           :price_lteq, 
-  #           :sorts,
-  #           {state_in: []}, 
-  #           {fee_payer_in: []}, 
-  #           {status_in: []})
-  # end
-
   def set_ransack
     @q = Item.ransack(params[:q])
   end
-
-
 end
